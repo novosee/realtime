63da29eae3b75efe9b9888ab58db3c420d091fe3
novosee <vivid.novosee@gmail.com>
2017-06-04 19:36:59 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,20 @@
 
 var container = document.getElementById('visualization');
-
-var dataset = new vis.DataSet();
-var options = {
-  start: vis.moment().add(-3, 'hours'), // changed so its faster
-  end: vis.moment(),
-};
-var graph2d = new vis.Graph2d(container, dataset, options);
-
+var database;
 
 firebasesetup();
 
+var dnow = new Date();
 
 
-var database;
+lodaTimeline(2017, 6, dnow.getDate(), 18, 0, 2017, 6, dnow.getDate(), 23, 1);
+lodaTimeline(2017, 6, dnow.getDate() - 1, 18, 0, 2017, 6, dnow.getDate() - 1, 23, 1);
+lodaTimeline(2017, 6, dnow.getDate() - 2, 18, 0, 2017, 6, dnow.getDate() - 2, 23, 1);
+lodaTimeline(2017, 6, dnow.getDate() - 3, 18, 0, 2017, 6, dnow.getDate() - 3, 23, 1);
+lodaTimeline(2017, 6, dnow.getDate() - 4, 18, 0, 2017, 6, dnow.getDate() - 4, 23, 1);
+
 
 function firebasesetup() {
-
   // Start Firebase
   var config = {
     apiKey: "AIzaSyA-VyZJOZVqXZj82wvVMkfJedDEhqXcIh8",
@@ -27,27 +25,30 @@ function firebasesetup() {
   };
   firebase.initializeApp(config);
   database = firebase.database();
-
-  loadAll();
 }
 
 
-
-function loadAll() {
+function lodaTimeline(syear, smonth, sday, shours, sminutes, eyear, emonth, eday, ehours, eminutes) {
+  var dataset = new vis.DataSet();
+  var now = new Date();
+  var utcoffset = now.getTimezoneOffset() * 60000;
+  var options = {
+    start: Date.UTC(syear, smonth - 1, sday, shours, sminutes, 0, 0) + utcoffset,//vis.moment().add(-3, 'hours'), // changed so its faster
+    end: Date.UTC(eyear, emonth - 1, eday, ehours, eminutes, 0, 0) + utcoffset,//vis.moment(),
+  };
+  var graph2d = new vis.Graph2d(container, dataset, options);
   var ref = database.ref("vivid");
-  //ref.orderByChild("time").startAt(1492863129709).endAt(1492864106661).on("child_added", gotAll, errData);
-  ref.orderByChild("time").limitToLast(250).on("child_added", gotAll, errData);
+  //Date.UTC(year, month(0-11), day, hours, minutes, seconds, millisec)
+  ref.orderByChild("time").startAt(Date.UTC(syear, smonth - 1, sday, shours, sminutes, 0, 0) + utcoffset).endAt(Date.UTC(eyear, emonth - 1, eday, ehours, eminutes, 0, 0) + utcoffset).on("child_added", gotAll, errData);
+
+  //ref.orderByChild("time").limitToLast(50).on("child_added", gotAll, errData);
   // The data comes back as an object
   function gotAll(gdata) {
     var sd = {};
     sd.x = new Date(gdata.val().time).toString();
     sd.y = gdata.val().count;
     dataset.add(sd);
-    //console.log(data);
-
-
-
-
+    //console.log(sd.x);
 
   }
 
